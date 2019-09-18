9f0da621d46695c6d956267d655d5a49d3e3009f
jc464492 <pagave.puana@my.jcu.edu.au>
2019-09-18 18:39:32 +1000

    Add function to determine score status

---
+++ prac_03/broken_score.py
@@ -0,0 +1,15 @@
+def determine_status():
+    """Get a numeric score and display its status."""
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+def determine_status(score):
+    """Determine the status of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"

36a67f6b1b68b03fe15a43f67d1775ee8c86c9a1
jc464492 <pagave.puana@my.jcu.edu.au>
2019-10-04 14:32:01 +1000

    Add Sort files based on extension

---
+++ prac_09/sort_files_1.py
@@ -0,0 +1,17 @@
+import os
+
+def main():
+    """Move files into folders with the same name as their extension."""
+    os.chdir("FilesToSort")
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+
+        extension = filename.split('.')[-1]
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            print.("{}/{}".format(extension, filename))
+        os.rename(filename, "{}/{}".format(extension, filename))
+
+main()
